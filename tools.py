534f81a079568841830c6074d078026476e9043b
ChunYeh Lu <window120909120909@gmail.com>
2019-10-24 02:53:13 +0800

    start to write NMC

--- tools.py
+++ tools.py
@@ -1,9 +1,15 @@
 import numpy as np
 
 
-def nmc():
+def nmc(t1, t2, ndim, obs_error, alpha, model, da, maxiter=5):
     """
     Using NMC method to construct background error covariance matrix.
     Parrish and Derber 1992, MWR
     """
-    pass
+    Pb = np.zeros((ndim,ndim))
+    np.fill_diagonal(Pb, obs_error)
+    
+    Pb_history = np.zeros((maxiter, Pb.size))
+    
+    for idx in range(maxiter):
+        pass
