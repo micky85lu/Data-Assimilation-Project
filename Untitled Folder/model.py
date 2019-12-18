6a6c89bc848610567225ac94f68397372e6e264e
ChunYeh Lu <window120909120909@gmail.com>
2019-12-18 19:03:43 +0800

    finish 3DEnVar and experiments

--- Untitled Folder/model.py
+++ Untitled Folder/model.py
@@ -28,7 +28,7 @@ def lorenz63_fdm(x0, ts, sigma=None, r=None, b=None):
     return np.vstack((x, y, z))
 
 
-def M(x, dt, sigma=None, r=None, b=None):
+def M63(x, dt, sigma=None, r=None, b=None):
     """tangent linear model for lorenz63_fdm"""
     if sigma is None:
         sigma = 10
@@ -72,3 +72,41 @@ def stability_index_lorenz63(x, y, z, sigma=None, r=None, b=None):
         # unstable case
         r[r<0] = 0
         return r.sum()
+    
+    
+def lorenz96_fdm(x0, ts, F=8):
+    dt = ts[1] - ts[0]
+    x = np.zeros((x0.size, ts.size))
+    x[:,0] = x0.ravel()
+    
+    for idx, time in enumerate(ts[1:]):
+        xn = x[:,idx]
+        xn_p1 = np.roll(xn, -1)  # x_{n+1}
+        xn_m1 = np.roll(xn, 1)  # x_{n-1}
+        xn_m2 = np.roll(xn, 2)  # x_{n-2}
+        x[:,idx+1] = xn + dt * ((xn_p1-xn_m2) * xn_m1 - xn + F)
+        
+    return x
+
+
+def M96(x, dt, F):
+    """tangent linear model for lorenz96_fdm"""
+    ndim = x.size
+    m = np.zeros((ndim, ndim))
+    np.fill_diagonal(m, 1 + dt * (F-1))
+    
+    up1 = np.roll(np.delete(x, 38), 1) * dt
+    m += np.diag(up1, k=1)
+    m[-1,0] = x[38] * dt
+    
+    low1 = (np.roll(np.delete(x, 1), -1) 
+            - np.roll(np.delete(x, 38), 1)) * dt
+    m += np.diag(low1, k=-1)
+    m[0,-1] = (x[1] - x[38]) * dt
+    
+    low2 = -np.delete(x, [0, 39]) * dt
+    m += np.diag(low2, k=-2)
+    m[0,-2] = -x[-1] * dt
+    m[1,-1] = -x[0] * dt
+    
+    return m
